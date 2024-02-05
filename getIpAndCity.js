0a098298cb3d270d8cb82f01a77b6367546015d3
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2024-02-05 10:09:43 +0900

    fishing

--- getIpAndCity.js
+++ getIpAndCity.js
@@ -2,15 +2,14 @@ export async function getIpAndCity() {
     const IP_API_KEY = '0974753c85db4cedab488bcf0a27d517';
     const data = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${IP_API_KEY}`)
         .then(results => {
-            if (!results.ok)
-                throw new Error(`네트워크가 원활하지 않습니다: ${results.statusText}`);
+            if (!results.ok) throw new Error(`네트워크가 원활하지 않습니다: \n${results.statusText}`);
             return results.json();
         });
 
     const locationData = await fetch(`http://ip-api.com/json/${data.ip}?lang=ko`)
         .then(res => res.json());
     return new PrivateInformation(
-        data.ip,
+        `${data.ip}`,
         `${locationData.city}, ${locationData.country}`
     );
 }
